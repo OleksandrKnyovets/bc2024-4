115dcd84493bc21464390851f12fff0780f6b3b9
OleksandrKnyovets bc2024-4 <sashakn2018@gmail.com>
2024-10-24 01:23:35 +0300

    переписав код, виправив помилки, додав перевірку параметрів

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const path = require('path');
 const superagent = require('superagent');
 
-// Налаштування параметрів командного рядка
+//налаштування параметрів командного рядка
 prog
     .option('-h, --host <host>', 'server host')
     .option('-p, --port <port>', 'server port')
@@ -16,83 +16,104 @@ prog
 prog.parse(process.argv);
 const opt = prog.opts();
 
-// Перевірка наявності каталогу кешу
-fs.promises.mkdir(opt.cache, { recursive: true })
-    .then(() => console.log('Cache directory is ready'))
-    .catch(error => console.error('Error creating cache directory:', error));
-
-// Функція обробки GET-запиту
-async function Get(code, fileN, res) {
-    try {
-        let data = await fs.promises.readFile(fileN); // Читаємо з кешу
-        res.writeHead(200, { 'Content-Type': 'image/jpeg' });
-        res.end(data);
-    } 
-    catch {
-        try {
-            const response = await superagent.get(`https://http.cat/${code}`); // Завантажуємо з http.cat
-            const data = response.body;
-            await fs.promises.writeFile(fileN, data); // Зберігаємо в кеш
-            res.writeHead(200, { 'Content-Type': 'image/jpeg' });
-            res.end(data);
-        } 
-        catch {
-            res.writeHead(404); // Не знайдено
-            res.end();
-        }
-    }
+//перевірка наявності обов'язкових параметрів
+if (!opt.host){
+    console.error('not a required parameter host');
+    process.exit(); 
 }
 
-// Функція обробки PUT-запиту
-async function Put(fileN, req, res) {
+if (!opt.port){
+    console.error('not a required parameter port');
+    process.exit(); 
+}
+
+if (!opt.cache){
+    console.error('not a required parameter cache');
+    process.exit(); //завершення програми 
+}
+
+fs.promises.mkdir(opt.cache, {recursive:true})
+    .then(()=>console.log('Cache directory is ready'))
+    .catch(error=>{
+        console.error('Error creating cache', error);
+        process.exit(); 
+    });
+
+//функція обробки GET-запиту
+function Get(code, fileX, res){
+    fs.promises.readFile(fileX) 
+        .then(data=>{
+            res.writeHead(200,{'Content-Type': 'image/jpeg'});
+            res.end(data);
+        })
+        .catch(()=>{
+            superagent.get(`https://http.cat/${code}`)
+                .then(response=>{
+                    const data = response.body;
+                    return fs.promises.writeFile(fileX, data); 
+                })
+                .then(data=>{ 
+                    res.writeHead(200, {'Content-Type': 'image/jpeg'});
+                    res.end(data);
+                })
+                .catch(err=>{
+                    console.error('Error fetching image:', err);
+                    res.writeHead(404); 
+                    res.end();
+                });
+        });
+}
+
+//функція обробки PUT-запиту
+function Put(fileX, req, res){
     const data = [];
-    req.on('data', chunk => data.push(chunk)); // Збираємо дані
-    req.on('end', async () => {
-        try {
-        await fs.promises.writeFile(fileN, Buffer.concat(data)); // Записуємо в кеш
-        res.writeHead(201); // Створено
-        res.end();
-        } 
-        catch {
-        res.writeHead(500); // Помилка сервера
-        res.end();
-        }
+    req.on('data', chunk =>data.push(chunk)); 
+    req.on('end', ()=>{
+        fs.promises.writeFile(fileX, Buffer.concat(data)) 
+            .then(()=>{
+                res.writeHead(201); 
+                res.end();
+            })
+            .catch(()=>{
+                res.writeHead(500); 
+                res.end();
+            });
     });
 }
 
-// Функція обробки DELETE-запиту
-async function Delete(fileN, res) {
-    try {
-        await fs.promises.unlink(fileN); // Видаляємо файл з кешу
-        res.writeHead(200); // ОК
-        res.end();
-    } 
-    catch {
-        res.writeHead(404); // Не знайдено
-        res.end();
-    }
+//функція обробки DELETE-запиту
+function Delete(fileX, res){
+    fs.promises.unlink(fileX) 
+        .then(()=>{
+            res.writeHead(200); 
+            res.end();
+        })
+        .catch(()=>{
+            res.writeHead(404);
+            res.end();
+        });
 }
 
-// Створення HTTP-сервера
-const server = http.createServer((req, res) => {
-    const code = req.url.slice(1); // Код з URL
-    const fileN = path.join(opt.cache, `${code}.jpg`); // Ім'я файлу кешу
+//створення HTTP-сервера
+const server = http.createServer((req, res)=>{
+    const code = req.url.slice(1);
+    const fileX = path.join(opt.cache, `${code}.jpg`);
 
-    switch (req.method) {
+    switch (req.method){
         case 'GET':
-            Get(code, fileN, res);
+            Get(code, fileX, res);
             break;
         case 'PUT':
-            Put(fileN, req, res);
+            Put(fileX, req, res);
             break;
         case 'DELETE':
-            Delete(fileN, res);
+            Delete(fileX, res);
             break;
         default:
-            res.writeHead(405); // Метод не підтримується
+            res.writeHead(405);
             res.end();
     }
 });
 
-// Запуск сервера
-server.listen(opt.port, opt.host, () => console.log(`Server running at http://${opt.host}:${opt.port}/`));
+//запуск сервера
+server.listen(opt.port, opt.host,()=>console.log(`Server running http://${opt.host}:${opt.port}/`));
